480e778fc290cb642fdcfa79d128a793ba5bdec1
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2025-10-04 17:02:12 -0300

    primeiro commit

--- q4/main.py
+++ q4/main.py
@@ -1,7 +1,7 @@
 # Questão 4 - Verificador de Números Pares e Ímpares
 # Crie uma função que receba um número inteiro e retorne se ele é par ou ímpar.
 
-def verificador_pares_impares(numero: int) -> int:
+def verificador_pares_impares(numero: int) -> str:
     if numero % 2 == 0:
         print("Par")
     else:
